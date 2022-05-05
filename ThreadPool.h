d0bac0c89a1cac53f7ce4a5711f5b5c0bd70b6da
qinyu <qinyufight123@126.com>
2022-05-05 23:18:41 +0800

    bug: 写错的bug展示

--- ThreadPool.h
+++ ThreadPool.h
@@ -9,6 +9,30 @@
 #include <pthread.h>
 #include <malloc.h>
 #include <string.h>
+#include <unistd.h>
+typedef struct Task {
+  void *(*func)(void*);
+  void *arg;
+}Task;
+
+struct ThreadPool {
+  // threadPool维护一个任务队列
+  Task *taskQ; // 任务队列
+  int queueFront; // 队首
+  int queueRear; // 队尾
+  int queueSize;
+  int queueCapacity;
+
+  // threadPool相关的线程数量
+  int maxThreads;
+  int minThreads;
+  // 线程相关
+  pthread_t *workingThreadIDs, managerThreadID;
+
+  // 互斥锁
+  pthread_mutex_t threadPoolMutex;
+};
+
 
 typedef struct ThreadPool ThreadPool;
 
--- main.cc
+++ main.cc
@@ -6,7 +6,17 @@
 
 
 int main() {
-  ThreadPool* threadPool;
-  createThreadPool(threadPool, 10, 20);
+  // 这里错误, 我们没有给threadPool进行初始化, 也就是没有进行给threadPool一个有效的地址, 所以默认是NULL, 那么如果NULL传递进去的话threadPool->属性初始化就是错误的
+  // todo(qinyu) 没有给threadPool初始化, 默认是NULL
+//  ThreadPool* threadPool;
+
+
+//  bug:nvalid application of 'sizeof' to an incomplete type 'ThreadPool', 这是因为struct ThreadPool定义在源文件中
+//  ThreadPool* threadPool = (ThreadPool *) malloc(sizeof(ThreadPool));
+
+//  如果避免不完整的类型, 我们必须把这个ThreadPool这个结构体类型进行对外暴漏. 比如我们现在放到头文件里面, 然后我们Include这个头文件, 编译器就知道完整的类型了
+// 但是这样的话, 我们的taskQ结构体也暴漏在外面了
+  ThreadPool *threadPool = (ThreadPool *) malloc(sizeof(ThreadPool));
+  createThreadPool(threadPool, 1, 1);
   return 0;
 };
